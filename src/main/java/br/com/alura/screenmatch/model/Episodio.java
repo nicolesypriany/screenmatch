aef4850a26473f30a7449773996ad69d3a7153ce
Nicole Notebook <nicolesypriany@gmail.com>
2025-05-26 10:28:30 -0300

    finalizando curso trabalhando com lambdas, streams e Spring Framework

--- src/main/java/br/com/alura/screenmatch/model/Episodio.java
+++ src/main/java/br/com/alura/screenmatch/model/Episodio.java
@@ -28,6 +28,46 @@ public class Episodio {
         }
     }
 
+    public Integer getTemporada() {
+        return temporada;
+    }
+
+    public void setTemporada(Integer temporada) {
+        this.temporada = temporada;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getNumeroEpisodio() {
+        return numeroEpisodio;
+    }
+
+    public void setNumeroEpisodio(Integer numeroEpisodio) {
+        this.numeroEpisodio = numeroEpisodio;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
     @Override
     public String toString() {
         return "temporada=" + temporada +
