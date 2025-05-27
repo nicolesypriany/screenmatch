ec3ae9ee53155ce640c5def507fe4f84215fe20b
Nicole Notebook <nicolesypriany@gmail.com>
2025-05-27 10:24:34 -0300

    aula mapeando as entidades

--- src/main/java/br/com/alura/screenmatch/model/DadosEpisodio.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosEpisodio.java
@@ -3,7 +3,6 @@ package br.com.alura.screenmatch.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.net.Inet4Address;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosEpisodio(@JsonAlias("Title") String titulo,
                             @JsonAlias("Episode") Integer numero,
